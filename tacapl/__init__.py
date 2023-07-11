a0e4546909b225f67d02c009902934f9006a907d
TouchIsNotAGoodCoder <64277067+Touchcreator@users.noreply.github.com>
2023-07-11 19:35:11 -0400

    fix the thing

--- tacapl/__init__.py
+++ tacapl/__init__.py
@@ -21,7 +21,7 @@ class Interpreter:
         global op_one, op_two
         tokens = statement.split(" ")
         command = tokens[0]
-        if skip_code == True and command != "end":
+        if skip_code and command != "end":
             command = ""
 
         commands = ["print", "set", "add", "subtract", "multiply", "divide", "if", "end", "input", "printsent", "ifstr"]
@@ -34,8 +34,8 @@ class Interpreter:
             elif value in self.variables:
                 # Print a variable value
                 print(self.variables[value])
-        if command == "printsent":
-            value = " ".join(tokens[1:])  # Combine all tokens after 'print' as the value
+        elif command == "printsent":
+            value = " ".join(tokens[1:])  # Combine all tokens after 'printsent' as the value
             if value.startswith('"') and value.endswith('"'):
                 # Print a string literal
                 print(value[1:-1], end="")
@@ -45,6 +45,7 @@ class Interpreter:
         elif command == "set":
             variable = tokens[1]
             value = " ".join(tokens[3:])  # Combine all tokens after 'set variable =' as the value
+            value = value[1:-1]
             self.variables[variable] = value
         elif command == "add":
             variable = tokens[1]
@@ -101,55 +102,35 @@ class Interpreter:
                 self.variables[variable] = op_one / op_two
             else:
                 print("Error: Division by zero!")
-        # Add more commands as needed
         elif command == "if":
             condition = tokens[1:]
             operation = condition[1]
             opop_one = condition[0]  # in case there's an error
             opop_two = condition[2]
-            try:
-                op_one = float(condition[0])
-            except ValueError:
-                op_one = float(self.variables[opop_one])
-            try:
-                op_two = float(condition[2])
-            except ValueError:
-                op_two = float(self.variables[opop_two])
+            op_one = opop_one  # Default to using variable name as the first operand
+            op_two = opop_two  # Default to using variable name as the second operand
 
-            # check for the answer
-            if operation == ">":
-                skip_code = not op_one > op_two
-            if operation == "<":
-                skip_code = not op_one < op_two
-            if operation == ">=":
-                skip_code = not op_one >= op_two
-            if operation == "<":
-                skip_code = not op_one <= op_two
-            if operation == "==":
-                skip_code = not op_one == op_two
-            if operation == "!=":
-                skip_code = not op_one != op_two
-        elif command == "ifstr":
-            condition = tokens[1:]
-            operation = condition[1]
-            opop_one = condition[0]  # in case there's an error
-            opop_two = condition[2]
-            if opop_one.startswith('"') and opop_one.endswith('"'):
-                # Print a string literal
-                op_one = opop_one
-            elif opop_one in self.variables:
+            # Check if the first operand is a variable and exists
+            if opop_one in self.variables:
                 op_one = self.variables[opop_one]
-            if opop_two.startswith('"') and opop_two.endswith('"'):
-                # Print a string literal
-                op_two = opop_two
-            elif opop_two in self.variables:
+
+            # Check if the second operand is a variable and exists
+            if opop_two in self.variables:
                 op_two = self.variables[opop_two]
-            # check for the answer
-            if operation == "==":
-                skip_code = not str(op_one) == str(op_two)
-                print(skip_code)
-            if operation == "!=":
-                skip_code = not str(op_one) != str(op_two)
+
+            # Check the condition based on the operation
+            if operation == ">":
+                skip_code = not (op_one > op_two)
+            elif operation == "<":
+                skip_code = not (op_one < op_two)
+            elif operation == ">=":
+                skip_code = not (op_one >= op_two)
+            elif operation == "<=":
+                skip_code = not (op_one <= op_two)
+            elif operation == "==":
+                skip_code = not (op_one == op_two)
+            elif operation == "!=":
+                skip_code = not (op_one != op_two)
         elif command == "end":
             skip_code = False
         elif command == "input":
