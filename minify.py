6ada411f55b9d1b39f377e3f35f09b0e54abff49
TouchIsNotAGoodCoder <64277067+Touchcreator@users.noreply.github.com>
2024-11-18 09:36:48 -0500

    optimize minfier for strings

--- minify.py
+++ minify.py
@@ -7,12 +7,30 @@ contents = filename.read()
 
 newThing = contents.strip()
 
-newThingAgain = ""
+i = 0
 
-for item in newThing:
-    if item != "\n":
-        newThingAgain += item
+listThing = []
 
-new.write(newThingAgain)
+for char in newThing:
+    listThing.append(char)
+
+quoteCount = 0
+
+print(listThing)
+
+while i < len(listThing):
+    if listThing[i] == "\"":
+        quoteCount+=1
+    if listThing[i] == "\n" and quoteCount % 2 == 0:
+        listThing.pop(i)
+        i-=1
+    i += 1
+
+stringThing = ""
+print(listThing)
+for thing in listThing:
+    stringThing += str(thing)
+
+new.write(stringThing)
 
 print("Done!")
